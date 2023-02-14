f196b5c2c37f520783adac4dcc618aa6d5406611
Yaroslav Kovaliv <tlcees@gmail.com>
2023-02-14 21:08:13 +0200

    added streaming from mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,76 @@
 var express = require("express");
 var path = require("path");
 const fs = require("fs");
+const mongodb = require("mongodb");
+require("dotenv").config();
+
+const dbPassword = process.env.dbPassword;
+const uri = `mongodb+srv://yaroslav:${dbPassword}@birthdayreminder.lyakbce.mongodb.net/?retryWrites=true&w=majority`;
 
 var router = express.Router();
+
+const videoPath = "./This is America.mp4";
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "/views/index.html"));
 });
 
-router.get("/video", (req, res) => {
-  const range = req.headers.range;
+router.get("/add-video", async function (req, res) {
+  const client = new mongodb.MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: mongodb.ServerApiVersion.v1,
+  });
 
+  await client.connect((err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+  });
+
+  const db = client.db("videos");
+  const bucket = new mongodb.GridFSBucket(db);
+  const videoUploadStream = bucket.openUploadStream(
+    videoPath.split("/").slice(-1).pop()
+  );
+  const videoReadStream = fs.createReadStream(videoPath);
+  videoReadStream.pipe(videoUploadStream);
+  client.close();
+
+  res.status(200).send("Done...");
+});
+
+router.get("/video", async function (req, res) {
+  const client = new mongodb.MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: mongodb.ServerApiVersion.v1,
+  });
+
+  client.connect((err) => {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+  });
+
+  const range = req.headers.range;
   if (!range) {
     res.status(400).send("Requires Range header");
   }
 
-  const videoPath = "This is America.mp4";
-  const videoSize = fs.statSync(videoPath).size;
+  const db = client.db("videos");
+  let video = await db.collection("fs.files").findOne();
 
-  const CHUNK_SIZE = 10 ** 6;
+  if (!video) {
+    res.status(404).send("No video uploaded!");
+    return;
+  }
+
+  const videoSize = video.length;
   const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+  const end = videoSize - 1;
 
   const contentLength = end - start + 1;
   const headers = {
@@ -32,8 +82,15 @@ router.get("/video", (req, res) => {
 
   res.writeHead(206, headers);
 
-  const videoStream = fs.createReadStream(videoPath, { start, end });
-  videoStream.pipe(res);
+  const bucket = new mongodb.GridFSBucket(db);
+  const downloadStream = bucket.openDownloadStreamByName(
+    "This is America.mp4",
+    {
+      start,
+    }
+  );
+
+  downloadStream.pipe(res);
 });
 
 module.exports = router;
